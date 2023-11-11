e86465f3c8532a8282bc24889421fbc251c9db38
afsheen minnotte <afsheenminnotte@gmail.com>
2023-11-11 14:25:06 +0400

    generate medication controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :medications
+  resources :medications, only: [:index, :show, :new, :create, :edit, :destroy]
   resources :frequencies, only: [:index, :show, :create, :update, :destroy]
   resources :intervals, only: [:index, :show, :create, :update, :destroy]
   resources :medication_frequencies, only: [:index, :show, :create, :update, :destroy]
   resources :reminders, only: [:index, :show, :create, :update, :destroy]
 
   root to: "pages#home"
-
 end
