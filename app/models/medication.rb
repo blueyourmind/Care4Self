4fdab9d41771452cd57df872e286084c2234da72
jaiveentaurah <jaiveen01@hotmail.com>
2023-11-13 21:30:18 +0400

    create views and set up routes

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -2,12 +2,17 @@
 class Medication < ApplicationRecord
   belongs_to :user
   belongs_to :interval
-  has_many :medication_frequencies
+  has_many :medication_frequencies, inverse_of: :medication, dependent: :destroy
+  has_many :frequencies, through: :medication_frequencies
+  accepts_nested_attributes_for :medication_frequencies
+  accepts_nested_attributes_for :interval
+
   validates :name, format: {with: /[a-zA-Z]/}, presence: true
   validates :instruction, presence: true
-  validates :duration, presence: true
+  # validates :duration, presence: true
   validates :quantity, presence: true
   validates :med_type, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
+  validates :user_id, presence: true
 end
--- app/models/medication_frequency.rb
+++ app/models/medication_frequency.rb
@@ -1,6 +1,6 @@
 class MedicationFrequency < ApplicationRecord
   belongs_to :medication
   belongs_to :frequency
-  has_many :reminders
-  
+  has_many :reminders, dependent: :destroy
+
 end
