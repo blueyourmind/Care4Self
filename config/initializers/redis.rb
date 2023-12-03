81ac41aef2243d32389cbc46a263be994670967e
Thierry Maclou <raymondmclou@gmail.com>
2023-12-03 21:08:06 +0400

    Committing redis  changes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,19 +1,19 @@
-# url = ENV["REDISCLOUD_URL"]
+url = ENV["REDISCLOUD_URL"]
 
-# if url
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: url }
-#   end
+if url
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
+  end
 
-#   Sidekiq.configure_client do |config|
-#     config.redis = { url: url }
-#   end
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
+  end
 
-#   $redis = Redis.new(url: url)
-# else
-#   $redis = Redis.new(url: 'redis://localhost:6379/0')
+  $redis = Redis.new(url: url)
+else
+  $redis = Redis.new(url: 'redis://localhost:6379/0')
 
-#   Sidekiq.configure_server do |config|
-#     config.redis = { url: 'redis://localhost:6379/0' }
-#   end
-# end
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
+end
