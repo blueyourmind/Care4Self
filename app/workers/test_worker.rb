ecf6e55665fe898b98797fdd1bbdc268b3528521
Thierry Maclou <raymondmclou@gmail.com>
2023-12-03 18:34:53 +0400

    heroku test 4

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,19 +1,19 @@
 class TestWorker
-  # include Sidekiq::Worker
+  include Sidekiq::Worker
 
-  # def perform(user_id, medication_name, notification_message)
-  #   user = User.find(user_id)
-  #   notification = Notification.new(
-  #     user: user,
-  #     recipient: user,
-  #     message: notification_message
-  #   )
+  def perform(user_id, medication_name, notification_message)
+    user = User.find(user_id)
+    notification = Notification.new(
+      user: user,
+      recipient: user,
+      message: notification_message
+    )
 
-  #   if notification.save
-  #     Rails.logger.info("Notification saved successfully!")
-  #     ActionCable.server.broadcast("notifications_channel", message: notification_message)
-  #   else
-  #     Rails.logger.error("Error saving notification: #{notification.errors.full_messages.join(', ')}")
-  #   end
-  # end
+    if notification.save
+      Rails.logger.info("Notification saved successfully!")
+      ActionCable.server.broadcast("notifications_channel", message: notification_message)
+    else
+      Rails.logger.error("Error saving notification: #{notification.errors.full_messages.join(', ')}")
+    end
+  end
 end
