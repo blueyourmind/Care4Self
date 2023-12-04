03124d5c8d88e03ef4ddbddfb655c581ab708a9e
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 05:32:09 +0400

    modification for heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,15 @@ module Care4self
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+
+    config.active_job.queue_adapter = :sidekiq
+
+    # Configure Redis with SSL verification bypass
     redis_url = "rediss://:pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f@ec2-63-34-38-39.eu-west-1.compute.amazonaws.com:26589"
-    redis = Redis.new(url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+    redis = Redis.new(url:"rediss://:pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f@ec2-63-34-38-39.eu-west-1.compute.amazonaws.com:26589" , ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+    # Set the Redis connection for your application
+    config.redis = redis
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -29,7 +36,7 @@ module Care4self
 # config.cache_store = :redis_store, ENV["REDIS_URL"] || "redis://localhost:6379/0/cache"
 
 
-    config.active_job.queue_adapter = :sidekiq
+    # config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
