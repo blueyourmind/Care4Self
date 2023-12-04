9d99bb4ca6504c6f06ba77bb22b376130c634714
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 16:24:02 +0400

    Update Redis cache store configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,21 +1,29 @@
-url = ENV["redis://default:oE5JupWrojIFHXeIr1O2fV7URUi8WSK6@redis-10704.c1.eu-west-1-3.ec2.cloud.redislabs.com:10704"]
+url = ENV["REDISCLOUD_URL"]
+password = "pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f"
 
 if url
-  # Assuming REDISCLOUD_URL is in the format: rediss://:your-password@hostname:port
+  redis_options = { url: url }
+
+  # Parse the URL to extract the password (if present)
+  uri = URI.parse(url)
+  redis_options[:password] = password
+
   Sidekiq.configure_server do |config|
-    config.redis = { url: url }
+    config.redis = redis_options
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = { url: url }
+    config.redis = redis_options
   end
 
-  $redis = Redis.new(url: url)
+  $redis = Redis.new(redis_options)
 else
-
-  $redis = Redis.new(url: 'redis://localhost:6379/0')
+  # Fallback to a local Redis server if REDISCLOUD_URL is not present
+  redis_options = { url: 'redis://localhost:6379/0' }
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
+    config.redis = redis_options
   end
+
+  $redis = Redis.new(redis_options)
 end
