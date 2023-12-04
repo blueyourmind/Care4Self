c8ffdd94ea3479b48e32dd83eba803371ae4dd99
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 08:57:31 +0400

    heroku update test

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,9 @@
+# config/application.rb
 require_relative "boot"
+require 'rails/all'
 require 'redis'
-require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups.reject { |group| group.in?(%w[assets]) })
-
 
 module Care4self
   class Application < Rails::Application
@@ -14,52 +12,27 @@ module Care4self
       generate.helper false
       generate.test_framework :test_unit, fixture: false
     end
-    # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 7.1
 
-    config.active_job.queue_adapter = :sidekiq
-# config/application.rb
+    # Configure Redis cache store
+    config.cache_store = :redis_cache_store, {
+      url: ENV["REDIS_URL"] || "redis://localhost:6379/0/cache"
+    }
 
-# ...
-
-config.cache_store = :redis_cache_store, { url: ENV["redis://default:oE5JupWrojIFHXeIr1O2fV7URUi8WSK6@redis-10704.c1.eu-west-1-3.ec2.cloud.redislabs.com:10704"] }
-
-
-
-
-
-# ...
-
-    # Configure Redis with SSL verification bypass
+    # Configure Redis connection with SSL verification bypass
     redis_url = "redis://:pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f@ec2-63-34-38-39.eu-west-1.compute.amazonaws.com:26589"
-    redis = Redis.new(url:"rediss://:pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f@ec2-63-34-38-39.eu-west-1.compute.amazonaws.com:26589" , ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+    redis = Redis.new(url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
     # Set the Redis connection for your application
     config.redis = redis
 
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
-    # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    # Configure autoload paths
     config.autoload_paths += %W(#{config.root}/app/notifications)
-# config/application.rb
-# config.cache_store = :redis_store, ENV["REDIS_URL"] || "redis://localhost:6379/0/cache"
 
-
-    # config.active_job.queue_adapter = :sidekiq
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Time zone configuration
     config.time_zone = 'Abu Dhabi'
     config.active_record.default_timezone = :local
     config.active_record.time_zone_aware_attributes = false
-
   end
-
 end
