386a4bd3386689bee9023e4cd94e14d3f1a882c7
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 07:46:46 +0400

    Update Redis configuration

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Care4self
 
 # ...
 
-config.cache_store = :redis_cache_store, { url: ENV["REDIS_URL"] }
+config.cache_store = :redis_cache_store, { url: ENV["redis://default:oE5JupWrojIFHXeIr1O2fV7URUi8WSK6@redis-10704.c1.eu-west-1-3.ec2.cloud.redislabs.com:10704"] }
+
 
 
 
