57c2c8d9647dfd102b695ad27fc4b5ffd5eaa5bc
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 04:22:53 +0400

    modified

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
-url = ENV["REDISCLOUD_URL"]
+url = ENV["redis://default:oE5JupWrojIFHXeIr1O2fV7URUi8WSK6@redis-10704.c1.eu-west-1-3.ec2.cloud.redislabs.com:10704"]
 
 if url
+  # Assuming REDISCLOUD_URL is in the format: rediss://:your-password@hostname:port
   Sidekiq.configure_server do |config|
     config.redis = { url: url }
   end
@@ -11,6 +12,7 @@ if url
 
   $redis = Redis.new(url: url)
 else
+
   $redis = Redis.new(url: 'redis://localhost:6379/0')
 
   Sidekiq.configure_server do |config|
