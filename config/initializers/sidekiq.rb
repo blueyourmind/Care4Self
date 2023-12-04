ff23bb6108b83112e6653e738b42503af68f2f52
Thierry Maclou <raymondmclou@gmail.com>
2023-12-04 15:55:32 +0400

    Update Redis cache store configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
-url = ENV["REDISCLOUD_URL"]
+url = "redis://default:oE5JupWrojIFHXeIr1O2fV7URUi8WSK6@redis-10704.c1.eu-west-1-3.ec2.cloud.redislabs.com:10704"
+password = "pe6dc62d20a26571050f70100b305e6f39b07d11b6e3c23394a3b0ee15a496c4f"
 
 if url
   redis_options = { url: url }
@@ -17,7 +18,7 @@ if url
 
   $redis = Redis.new(redis_options)
 else
-  # Fallback to local Redis server if REDISCLOUD_URL is not present
+  # Fallback to a local Redis server if REDIS_URL is not present
   redis_options = { url: 'redis://localhost:6379/0' }
 
   Sidekiq.configure_server do |config|
