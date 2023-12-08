c2fb726aa3d7de85c9d8bc2714a37d431e99f392
Thierry Maclou <raymondmclou@gmail.com>
2023-12-08 22:08:32 +0400

    final_modifications

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,8 @@
 // Entry point for the build script in your package.json
-// Entry point for the build script in your package.json
-import Rails from "@rails/ujs";
 import "@hotwired/turbo-rails";
-import "bootstrap";
 import "./controllers";
-
-window.Rails = Rails;
-
+import "bootstrap";
 //= require jquery
 //= require bootstrap
 //= require rails-ujs
 //= require_tree .
-//= require rails-ujs
-
--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -1,32 +1,14 @@
-// app/javascript/controllers/notification_controller.js
-import { Controller } from 'stimulus';
+// // app/javascript/controllers/notification_controller.js
+// import { Controller } from 'stimulus';
 
-export default class extends Controller {
-  static targets = ['notification'];
+// export default class extends Controller {
+//   static targets = ['notification'];
 
-    close() {
-      console.log("Close action executed");
-      this.notificationTarget.classList.add("closed");
-  }
+//   close() {
+//     console.log("Close action executed");
+//     this.notificationTarget.classList.add("closed");
 
-  async closeNotification(notificationId) {
-    try {
-      const response = await fetch(`/notifications/${notificationId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': document.head.querySelector('[name="csrf-token"]').content,
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error closing notification: ${response.statusText}`);
-      }
-
-      this.notificationTarget.classList.add('closed');
-      // Add additional logic here if needed
-    } catch (error) {
-      console.error(error);
-    }
-  }
-}
+//     // Set the variable to true when the notification is closed
+//     this.application.setVariable('notification_closed', true);
+//   }
+// }
