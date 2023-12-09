dfb2366377c926d97492356360edb1f544b2dc4b
Thierry Maclou <raymondmclou@gmail.com>
2023-12-09 10:14:02 +0400

    sidekiq_fix

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,10 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: 'sidekiq', pool_size: 5 }
-  config.average_scheduled_poll_interval = 15 # seconds
-  config.time_zone = 'Port louis'
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/0', namespace: 'sidekiq', pool_size: 5 }
-  config.time_zone = 'Port louis'
+  config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
 end
